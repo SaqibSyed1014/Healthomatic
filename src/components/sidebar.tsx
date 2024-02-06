3bd49e39b6ebf8a5a1f0d0aaa5fa6545b1c8ebe2
Syed Saqib Ali <Saqibsyed1014@gmail.com>
2024-02-06 12:12:55 +0500

    extra sidebar links removed

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -6,17 +6,8 @@ import { useEffect, useState } from "react";
 import {
   HiAdjustments,
   HiChartPie,
-  HiChartSquareBar,
-  HiClipboard,
   HiCog,
-  HiCollection,
-  HiInboxIn,
-  HiInformationCircle,
-  HiLockClosed,
   HiSearch,
-  HiShoppingBag,
-  HiUsers,
-  HiViewGrid,
 } from "react-icons/hi";
 
 import { useSidebarContext } from "../context/SidebarContext";
@@ -27,16 +18,16 @@ const ExampleSidebar: FC = function () {
     useSidebarContext();
 
   const [currentPage, setCurrentPage] = useState("");
-  const [isEcommerceOpen, setEcommerceOpen] = useState(true);
-  const [isUsersOpen, setUsersOpen] = useState(true);
+  // const [isEcommerceOpen, setEcommerceOpen] = useState(true);
+  // const [isUsersOpen, setUsersOpen] = useState(true);
 
   useEffect(() => {
     const newPage = window.location.pathname;
 
     setCurrentPage(newPage);
-    setEcommerceOpen(newPage.includes("/e-commerce/"));
-    setUsersOpen(newPage.includes("/users/"));
-  }, [setCurrentPage, setEcommerceOpen, setUsersOpen]);
+    // setEcommerceOpen(newPage.includes("/e-commerce/"));
+    // setUsersOpen(newPage.includes("/users/"));
+  }, [setCurrentPage]);
 
   return (
     <div
@@ -79,167 +70,167 @@ const ExampleSidebar: FC = function () {
                 >
                   Course Creation
                 </Sidebar.Item>
-                <Sidebar.Item
-                  href="/"
-                  icon={HiChartPie}
-                  className={
-                    "/" === currentPage ? "bg-gray-100 dark:bg-gray-700" : ""
-                  }
-                >
-                  Dashboard
-                </Sidebar.Item>
-                <Sidebar.Item
-                  href="/kanban"
-                  icon={HiViewGrid}
-                  className={
-                    "/kanban" === currentPage
-                      ? "bg-gray-100 dark:bg-gray-700"
-                      : ""
-                  }
-                >
-                  Kanban
-                </Sidebar.Item>
-                <Sidebar.Item
-                  href="/mailing/inbox"
-                  icon={HiInboxIn}
-                  label="3"
-                  className={
-                    "/mailing/inbox" === currentPage
-                      ? "bg-gray-100 dark:bg-gray-700"
-                      : ""
-                  }
-                >
-                  Inbox
-                </Sidebar.Item>
-                <Sidebar.Collapse
-                  icon={HiShoppingBag}
-                  label="E-commerce"
-                  open={isEcommerceOpen}
-                >
-                  <Sidebar.Item
-                    href="/e-commerce/products"
-                    className={
-                      "/e-commerce/products" === currentPage
-                        ? "bg-gray-100 dark:bg-gray-700"
-                        : ""
-                    }
-                  >
-                    Products
-                  </Sidebar.Item>
-                  <Sidebar.Item
-                    href="/e-commerce/billing"
-                    className={
-                      "/e-commerce/billing" === currentPage
-                        ? "bg-gray-100 dark:bg-gray-700"
-                        : ""
-                    }
-                  >
-                    Billing
-                  </Sidebar.Item>
-                  <Sidebar.Item
-                    href="/e-commerce/invoice"
-                    className={
-                      "/e-commerce/invoice" === currentPage
-                        ? "bg-gray-100 dark:bg-gray-700"
-                        : ""
-                    }
-                  >
-                    Invoice
-                  </Sidebar.Item>
-                </Sidebar.Collapse>
-                <Sidebar.Collapse
-                  icon={HiUsers}
-                  label="Users"
-                  open={isUsersOpen}
-                >
-                  <Sidebar.Item
-                    href="/users/list"
-                    className={
-                      "/users/list" === currentPage
-                        ? "bg-gray-100 dark:bg-gray-700"
-                        : ""
-                    }
-                  >
-                    Users list
-                  </Sidebar.Item>
-                  <Sidebar.Item
-                    href="/users/profile"
-                    className={
-                      "/users/profile" === currentPage
-                        ? "bg-gray-100 dark:bg-gray-700"
-                        : ""
-                    }
-                  >
-                    Profile
-                  </Sidebar.Item>
-                  <Sidebar.Item
-                    href="/users/feed"
-                    className={
-                      "/users/feed" === currentPage
-                        ? "bg-gray-100 dark:bg-gray-700"
-                        : ""
-                    }
-                  >
-                    Feed
-                  </Sidebar.Item>
-                  <Sidebar.Item
-                    href="/users/settings"
-                    className={
-                      "/users/settings" === currentPage
-                        ? "bg-gray-100 dark:bg-gray-700"
-                        : ""
-                    }
-                  >
-                    Settings
-                  </Sidebar.Item>
-                </Sidebar.Collapse>
-                <Sidebar.Collapse icon={HiChartSquareBar} label="Pages">
-                  <Sidebar.Item href="/pages/pricing">Pricing</Sidebar.Item>
-                  <Sidebar.Item href="/pages/maintenance">
-                    Maintenace
-                  </Sidebar.Item>
-                  <Sidebar.Item href="/pages/404">404 not found</Sidebar.Item>
-                  <Sidebar.Item href="/pages/500">
-                    500 server error
-                  </Sidebar.Item>
-                </Sidebar.Collapse>
-                <Sidebar.Collapse icon={HiLockClosed} label="Authentication">
-                  <Sidebar.Item href="/authentication/sign-in">
-                    Sign in
-                  </Sidebar.Item>
-                  <Sidebar.Item href="/authentication/sign-up">
-                    Sign up
-                  </Sidebar.Item>
-                  <Sidebar.Item href="/authentication/forgot-password">
-                    Forgot password
-                  </Sidebar.Item>
-                  <Sidebar.Item href="/authentication/reset-password">
-                    Reset password
-                  </Sidebar.Item>
-                  <Sidebar.Item href="/authentication/profile-lock">
-                    Profile lock
-                  </Sidebar.Item>
-                </Sidebar.Collapse>
-              </Sidebar.ItemGroup>
-              <Sidebar.ItemGroup>
-                <Sidebar.Item
-                  href="https://github.com/themesberg/flowbite-react/"
-                  icon={HiClipboard}
-                >
-                  Docs
-                </Sidebar.Item>
-                <Sidebar.Item
-                  href="https://flowbite-react.com/"
-                  icon={HiCollection}
-                >
-                  Components
-                </Sidebar.Item>
-                <Sidebar.Item
-                  href="https://github.com/themesberg/flowbite-react/issues"
-                  icon={HiInformationCircle}
-                >
-                  Help
-                </Sidebar.Item>
+                {/*<Sidebar.Item*/}
+                {/*  href="/"*/}
+                {/*  icon={HiChartPie}*/}
+                {/*  className={*/}
+                {/*    "/" === currentPage ? "bg-gray-100 dark:bg-gray-700" : ""*/}
+                {/*  }*/}
+                {/*>*/}
+                {/*  Dashboard*/}
+                {/*</Sidebar.Item>*/}
+              {/*  <Sidebar.Item*/}
+              {/*    href="/kanban"*/}
+              {/*    icon={HiViewGrid}*/}
+              {/*    className={*/}
+              {/*      "/kanban" === currentPage*/}
+              {/*        ? "bg-gray-100 dark:bg-gray-700"*/}
+              {/*        : ""*/}
+              {/*    }*/}
+              {/*  >*/}
+              {/*    Kanban*/}
+              {/*  </Sidebar.Item>*/}
+              {/*  <Sidebar.Item*/}
+              {/*    href="/mailing/inbox"*/}
+              {/*    icon={HiInboxIn}*/}
+              {/*    label="3"*/}
+              {/*    className={*/}
+              {/*      "/mailing/inbox" === currentPage*/}
+              {/*        ? "bg-gray-100 dark:bg-gray-700"*/}
+              {/*        : ""*/}
+              {/*    }*/}
+              {/*  >*/}
+              {/*    Inbox*/}
+              {/*  </Sidebar.Item>*/}
+              {/*  <Sidebar.Collapse*/}
+              {/*    icon={HiShoppingBag}*/}
+              {/*    label="E-commerce"*/}
+              {/*    open={isEcommerceOpen}*/}
+              {/*  >*/}
+              {/*    <Sidebar.Item*/}
+              {/*      href="/e-commerce/products"*/}
+              {/*      className={*/}
+              {/*        "/e-commerce/products" === currentPage*/}
+              {/*          ? "bg-gray-100 dark:bg-gray-700"*/}
+              {/*          : ""*/}
+              {/*      }*/}
+              {/*    >*/}
+              {/*      Products*/}
+              {/*    </Sidebar.Item>*/}
+              {/*    <Sidebar.Item*/}
+              {/*      href="/e-commerce/billing"*/}
+              {/*      className={*/}
+              {/*        "/e-commerce/billing" === currentPage*/}
+              {/*          ? "bg-gray-100 dark:bg-gray-700"*/}
+              {/*          : ""*/}
+              {/*      }*/}
+              {/*    >*/}
+              {/*      Billing*/}
+              {/*    </Sidebar.Item>*/}
+              {/*    <Sidebar.Item*/}
+              {/*      href="/e-commerce/invoice"*/}
+              {/*      className={*/}
+              {/*        "/e-commerce/invoice" === currentPage*/}
+              {/*          ? "bg-gray-100 dark:bg-gray-700"*/}
+              {/*          : ""*/}
+              {/*      }*/}
+              {/*    >*/}
+              {/*      Invoice*/}
+              {/*    </Sidebar.Item>*/}
+              {/*  </Sidebar.Collapse>*/}
+              {/*  <Sidebar.Collapse*/}
+              {/*    icon={HiUsers}*/}
+              {/*    label="Users"*/}
+              {/*    open={isUsersOpen}*/}
+              {/*  >*/}
+              {/*    <Sidebar.Item*/}
+              {/*      href="/users/list"*/}
+              {/*      className={*/}
+              {/*        "/users/list" === currentPage*/}
+              {/*          ? "bg-gray-100 dark:bg-gray-700"*/}
+              {/*          : ""*/}
+              {/*      }*/}
+              {/*    >*/}
+              {/*      Users list*/}
+              {/*    </Sidebar.Item>*/}
+              {/*    <Sidebar.Item*/}
+              {/*      href="/users/profile"*/}
+              {/*      className={*/}
+              {/*        "/users/profile" === currentPage*/}
+              {/*          ? "bg-gray-100 dark:bg-gray-700"*/}
+              {/*          : ""*/}
+              {/*      }*/}
+              {/*    >*/}
+              {/*      Profile*/}
+              {/*    </Sidebar.Item>*/}
+              {/*    <Sidebar.Item*/}
+              {/*      href="/users/feed"*/}
+              {/*      className={*/}
+              {/*        "/users/feed" === currentPage*/}
+              {/*          ? "bg-gray-100 dark:bg-gray-700"*/}
+              {/*          : ""*/}
+              {/*      }*/}
+              {/*    >*/}
+              {/*      Feed*/}
+              {/*    </Sidebar.Item>*/}
+              {/*    <Sidebar.Item*/}
+              {/*      href="/users/settings"*/}
+              {/*      className={*/}
+              {/*        "/users/settings" === currentPage*/}
+              {/*          ? "bg-gray-100 dark:bg-gray-700"*/}
+              {/*          : ""*/}
+              {/*      }*/}
+              {/*    >*/}
+              {/*      Settings*/}
+              {/*    </Sidebar.Item>*/}
+              {/*  </Sidebar.Collapse>*/}
+              {/*  <Sidebar.Collapse icon={HiChartSquareBar} label="Pages">*/}
+              {/*    <Sidebar.Item href="/pages/pricing">Pricing</Sidebar.Item>*/}
+              {/*    <Sidebar.Item href="/pages/maintenance">*/}
+              {/*      Maintenace*/}
+              {/*    </Sidebar.Item>*/}
+              {/*    <Sidebar.Item href="/pages/404">404 not found</Sidebar.Item>*/}
+              {/*    <Sidebar.Item href="/pages/500">*/}
+              {/*      500 server error*/}
+              {/*    </Sidebar.Item>*/}
+              {/*  </Sidebar.Collapse>*/}
+              {/*  <Sidebar.Collapse icon={HiLockClosed} label="Authentication">*/}
+              {/*    <Sidebar.Item href="/authentication/sign-in">*/}
+              {/*      Sign in*/}
+              {/*    </Sidebar.Item>*/}
+              {/*    <Sidebar.Item href="/authentication/sign-up">*/}
+              {/*      Sign up*/}
+              {/*    </Sidebar.Item>*/}
+              {/*    <Sidebar.Item href="/authentication/forgot-password">*/}
+              {/*      Forgot password*/}
+              {/*    </Sidebar.Item>*/}
+              {/*    <Sidebar.Item href="/authentication/reset-password">*/}
+              {/*      Reset password*/}
+              {/*    </Sidebar.Item>*/}
+              {/*    <Sidebar.Item href="/authentication/profile-lock">*/}
+              {/*      Profile lock*/}
+              {/*    </Sidebar.Item>*/}
+              {/*  </Sidebar.Collapse>*/}
+              {/*</Sidebar.ItemGroup>*/}
+              {/*<Sidebar.ItemGroup>*/}
+              {/*  <Sidebar.Item*/}
+              {/*    href="https://github.com/themesberg/flowbite-react/"*/}
+              {/*    icon={HiClipboard}*/}
+              {/*  >*/}
+              {/*    Docs*/}
+              {/*  </Sidebar.Item>*/}
+              {/*  <Sidebar.Item*/}
+              {/*    href="https://flowbite-react.com/"*/}
+              {/*    icon={HiCollection}*/}
+              {/*  >*/}
+              {/*    Components*/}
+              {/*  </Sidebar.Item>*/}
+              {/*  <Sidebar.Item*/}
+              {/*    href="https://github.com/themesberg/flowbite-react/issues"*/}
+              {/*    icon={HiInformationCircle}*/}
+              {/*  >*/}
+              {/*    Help*/}
+              {/*  </Sidebar.Item>*/}
               </Sidebar.ItemGroup>
             </Sidebar.Items>
           </div>
